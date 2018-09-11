d839dd4fc5cda7dc06c01442f7ed3d7756c3f06f
julien <julien.moratalla@gmail.com>
2018-09-11 20:36:16 +0200

    exercise-1 in process

--- exercise-1/string.js
+++ exercise-1/string.js
@@ -15,32 +15,66 @@ function capitalize(str) {
 	return str;
 }
 
-function camelCase() {
+function camelCase(str) {
+	str = capitalize(str);
+	str = str.replace(/ /g, '');
 
+	return str;
 }
 
-function snake_case() {
+function snake_case(str) {
+	str = str.toLowerCase();
+	str = str.replace(/ /g, '-');
+
+	return str;
+}
+
+function prop_access(str, prop = {}) {
+	prop.unshift(str);
+	prop['type']['name'] = "chien";
 	
 }
 
-function prop_access() {
-	
+function leet(str) {
+	let res = "";
+
+	if (str.indexOf('a') > -1) {
+	  	res = str.replace(/a/g, "4");
+	}
+	if (str.indexOf('e') > -1) {
+	  	res = str.replace(/e/g, "3");
+	}
+	if (str.indexOf('i') > -1) {
+	  	res = str.replace(/i/g, "1");
+	}
+	if (str.indexOf('o') > -1) {
+	  	res = str.replace(/o/g, "o");
+	}
+	if (str.indexOf('u') > -1) {
+	  	res = str.replace(/u/g, "_");
+	}
+	if (str.indexOf('y') > -1) {
+	  	res = str.replace(/y/g, "7")
+	}
+
+	return res;
+
 }
 
-function leet() {
-
+function verlan(str) {
+	return str.split("").reverse().join("");
 }
 
-function verlan() {
-
-}
-
-function yoda() {
-
+function yoda(str) {
+	return str.reverse().join("");
 }
 
 function vig() {
 
 }
 
-console.log(capitalize("hello world"));
+console.log(yoda("Hello World"));
+
+
+
+
